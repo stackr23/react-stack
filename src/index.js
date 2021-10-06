e4ec21bf93002066970a9fd2382c067715452bd1
DoubleU23 <5007050+DoubleU23@users.noreply.github.com>
2021-10-06 15:40:42 +0200

    refactor: change `App` component to TSX and apply `AppProps` interface

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import App from './App.jsx'
+import App from './App'
 
 
 const rootElement = document.getElementById('app-root') || document.getElementById('app')
