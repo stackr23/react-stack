8de4ad84372311225e2c765d758c5b940cf41f18
DoubleU23 <5007050+DoubleU23@users.noreply.github.com>
2021-10-06 15:42:10 +0200

    add `JsTestComponent` for mixed codebase (TS+JS)

---
+++ src/components/JsTestComponent/JsTestComponent.jsx
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react'
+
+const JsTestComponent = (test) => {
+  return <>{test + ''}</>
+  // return props.testparam ? <>{props.testparam}</> : <>{'JSTESTCOMPONENT'}</>
+}
+
+export default JsTestComponent
+
